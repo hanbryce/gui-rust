034d169ef8212b7a4c2162b08fdc59f19b61324a
Bryce Han <bryce.han@acs.com.hk>
2022-11-08 14:35:55 +0800

    Simple Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{executor, Application, Command, Element, Settings, Text};
+
+pub fn main() -> iced::Result {
+    Hello::run(Settings::default())
+}
+
+struct Hello;
+
+impl Application for Hello {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
+        (Hello, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Black J")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
 }

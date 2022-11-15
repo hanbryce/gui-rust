b1d4ca020bb48f7d8f031f8c6b32178166627024
Bryce Han <bryce.han@acs.com.hk>
2022-11-15 11:38:50 +0800

    Real-time tracking interaction

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-use iced::{executor, Application, Command, Element, Settings, Subscription, Alignment};
+use iced::{executor, Application, Command, Element, Settings, Subscription, Alignment, Theme, time as iced_time};
 use time::{OffsetDateTime};
-use iced::widget::{Text, Button, button, Column};
+use iced::widget::{Text, Button, Column};
 
 pub fn main() -> iced::Result {
     Board::run(Settings::default())
 }
 
 struct Board {
-    now: OffsetDateTime,
-    button: button::State
+    now_click: OffsetDateTime,
+    now_tick: OffsetDateTime
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
+    Click,
     Tick,
 }
 
@@ -20,11 +21,13 @@ impl Application for Board {
     type Executor = executor::Default;
     type Message = Message;
     type Flags = ();
+    type Theme = Theme;
 
     fn new(_flags: ()) -> (Self, Command<Message>) {
         (
-            Self { now: OffsetDateTime::now_utc(),
-                button: button::State::new()
+            Self { 
+                now_click: OffsetDateTime::now_utc(),
+                now_tick: OffsetDateTime::now_utc()
             },
             Command::none(),
         )
@@ -37,31 +40,33 @@ impl Application for Board {
     fn update(&mut self, message: Message) -> Command<Message> {
 
         match message {
+            Message::Click => {
+                self.now_click = OffsetDateTime::now_utc()
+            },
             Message::Tick => {
-                self.now = OffsetDateTime::now_utc()
-            }
+                self.now_tick = OffsetDateTime::now_utc()
+            },
         }
 
         Command::none()
     }
 
-    fn view(&mut self) -> Element<Message> {
+    fn view(&self) -> Element<Message> {
         Column::new()
-        .push(Text::new(self.now.to_string()))
-        .push(Button::new(&mut self.button,Text::new("Up"))
-            .on_press(Message::Tick)
+        .push(Text::new(self.now_click.to_string()))
+        .push(Button::new(Text::new("Up"))
+            .on_press(Message::Click)
         )
+        .push(Text::new(self.now_tick.to_string()))
         .padding(20)
         .align_items(Alignment::Center)
         .into()
     }
 
-    // fn subscription(&self) -> Subscription<Message> {
-    //     iced::time::every(std::time::Duration::from_millis(500)).map(|_| {
-    //         Message::Tick(
-    //             time::OffsetDateTime::now_utc(),
-    //         )
-    //     })
-    // }
+    fn subscription(&self) -> Subscription<Message> {
+        iced_time::every(std::time::Duration::from_millis(500)).map(|_| {
+            Message::Tick
+        })
+    }
 
 }
